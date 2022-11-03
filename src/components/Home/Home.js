ecfeb8c0fa0f7c1be7e8987765f0736db465f158
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-04 01:43:10 +0600

    user data show on home and get permission to deleting user data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
+    // console.log(users)
+
+    const handleDelete = user => {
+        const agree = window.confirm(`Are you sure you want to delete ${user.name}`);
+        if(agree){
+            console.log('deleting user with id:',user._id);
+        }
+    }
     return (
         <div>
-            <h1>Users</h1>
+            <h1>Users: {users.length}</h1>
+            <div>
+                {
+                    users.map(user => <p 
+                        key={user._id}
+                        >{user.name} <br /> {user.address} <br /> {user.email} <button onClick={() => handleDelete(user)}>x</button></p> )
+                }
+            </div>
         </div>
     );
 };
